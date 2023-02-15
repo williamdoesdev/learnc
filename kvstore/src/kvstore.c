2ebac3aabc1e53338de0a8a4edf61e0baee3b526
William <william@wikiplantia.com>
2023-02-14 18:55:50 -0600

    working on learning the jansson library

--- kvstore/src/kvstore.c
+++ kvstore/src/kvstore.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <json.h>
+#include <jansson.h>
 
 int main(int argc, char *argv[]) {
    
@@ -7,6 +7,21 @@ int main(int argc, char *argv[]) {
         printf("no args");
     } else if (argc == 3) {
         printf("2 args");
+
+        json_t *root = json_object();
+        json_object_set_new(root, "name", json_string("Alice"));
+        json_object_set_new(root, "age", json_integer(25));
+
+        // serialize the JSON object
+        char *json_str = json_dumps(root, JSON_ENCODE_ANY);
+
+        // print the serialized JSON
+        printf("%s\n", json_str);
+
+        // free the JSON object and the serialized string
+        json_decref(root);
+        free(json_str);
+
     } else {
         printf("Usage: kvstore <command> [arguments]");
         return 1;
